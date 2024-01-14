f54d4db7aab682304ea9d7e7ad7e57559250a46e
bopsicle-0147 <110119290+bopsicle-0147@users.noreply.github.com>
2024-01-13 22:56:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const youtubeChannel = document.getElementById("youtube channel");
+const twitterAccount = document.getElementById("twitter account");
+const learnMore = document.getElementById("Learn More Button");
+
+youtubeChannel.addEventListener("click", openYtNewTab);
+twitterAccount.addEventListener("click", openTwitterNewTab);
+learnMore.addEventListener("click", openYtNewTab);
+
+
+function openYtNewTab() {
+    window.open("https://youtube.com/@oliviajoyryan", "_blank");
+}
+
+function openTwitterNewTab() {
+    window.open("https://twitter.com/@oliviajoyryan", "_blank");
+}
